7b09b92f4a19dda14a14ac2862847424c203f5b4
gunsterpsp <101326389+gunsterpsp@users.noreply.github.com>
2022-06-14 22:36:09 +0800

    first commit

---
+++ phpwithemail/register.php
@@ -0,0 +1,176 @@
+<?php 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+session_start(); ?>
+<?php
+
+    include('connect/connection.php');
+
+    if(isset($_POST["register"])){
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        $check_query = mysqli_query($connect, "SELECT * FROM login where email ='$email'");
+        $rowCount = mysqli_num_rows($check_query);
+
+        if(!empty($email) && !empty($password)){
+            if($rowCount > 0){
+                ?>
+                <script>
+                    alert("User with email already exist!");
+                </script>
+                <?php
+            }else{
+                $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+                $result = mysqli_query($connect, "INSERT INTO login (email, password, status) VALUES ('$email', '$password_hash', 0)");
+    
+                if($result){
+                    $otp = rand(100000,999999);
+                    $_SESSION['otp'] = $otp;
+                    $_SESSION['mail'] = $email;
+
+					$recipient = $custUser["email"];
+					$subject = "Order Confirmation. Proceed To Pick Up Location - WC Overrun.";
+					
+					require "phpmailer/PhpMailer/PHPMailer.php";
+					require "phpmailer/PhpMailer/SMTP.php";
+					require "phpmailer/PhpMailer/Exception.php";
+					
+					$mail = new PHPMailer();
+					$mail->isSMTP();
+					$mail->Host = "smtp.gmail.com";
+					$mail->SMTPAuth = true;
+					$mail->SMTPSecure = "ssl"; //tls
+					$mail->Username = "rikasan192@gmail.com";
+					$mail->Password = "tdtqqgpktmmtcwua";
+					$mail->setfrom("rikasan192@gmail.com");
+					$mail->Subject = "This is your OTP verification code";
+					$mail->Port = 465; //587
+					$mail->IsHTML(true);
+					$mail->Body = '';			   
+					$mail->addAddress("maebear143@gmail.com");	
+					$mail->addcc("maebear143@gmail.com");		
+                    $mail->Body="<p>Dear user, </p> <p>Your verify OTP code is $otp <br></p>";
+                    
+    
+                            if(!$mail->send()){
+                                ?>
+                                    <script>
+                                        alert("<?php echo "Register Failed, Invalid Email "?>");
+                                    </script>
+                                <?php
+                            }else{
+                                ?>
+                                <script>
+                                    alert("<?php echo "Register Successfully, OTP sent to " . $email ?>");
+                                    window.location.replace('verification.php');
+                                </script>
+                                <?php
+                            }
+                }
+            }
+        }
+    }
+
+?>
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css" />
+<!------ Include the above in your HEAD tag ---------->
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+
+    <link rel="stylesheet" href="style.css">
+
+    <link rel="icon" href="Favicon.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+
+    <title>Register Form</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-light navbar-laravel">
+    <div class="container">
+        <a class="navbar-brand" href="#">Register Form</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php" >Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="register.php" style="font-weight:bold; color:black; text-decoration:underline">Register</a>
+                </li>
+            </ul>
+
+        </div>
+    </div>
+</nav>
+
+<main class="login-form">
+    <div class="cotainer">
+        <div class="row justify-content-center">
+            <div class="col-md-6 mt-5">
+                <div class="card">
+                    <div class="card-header">Register</div>
+                    <div class="card-body">
+                        <form action="#" method="POST" name="register">
+                            <div class="form-group row">
+                                <label for="email_address" class="col-md-4 col-form-label text-md-right">E-Mail Address</label>
+                                <div class="col-md-6">
+                                    <input type="text" id="email_address" class="form-control" name="email" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="password" class="col-md-4 col-form-label text-md-right">Password</label>
+                                <div class="col-md-6">
+                                    <input type="password" id="password" class="form-control" name="password" required>
+                                    
+                                </div>
+                            </div>
+
+                            <div class="col-md-6 offset-md-4">
+                                <input type="submit" value="Register" name="register">
+                            </div>
+                    </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+</main>
+</body>
+</html>
+<script>
+    const toggle = document.getElementById('togglePassword');
+    const password = document.getElementById('password');
+
+    toggle.addEventListener('click', function(){
+        if(password.type === "password"){
+            password.type = 'text';
+        }else{
+            password.type = 'password';
+        }
+        this.classList.toggle('bi-eye');
+    });
+</script>
